2761fcde8ef3edb2450bb1f9ee9c8f3a899eee61
coursework <coursework@coursework.csc.tntech.edu>
2019-11-25 22:52:04 -0600

    Added setup script

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $(whoami) != "root" ] 
+then
+	echo "I must be run as root!"
+	exit
+fi
+
+echo "Success! Setting up environment now"
+
+rm /var/www/html/*
+
+mysql -u root -pcoursework < database.txt
+
+cp index.html /var/www/html
+cp test.php /var/www/html
